c5395eb2af90b353b674e5eb089aaf0dbc4cfb65
mallowlabs <mallowlabs@gmail.com>
2014-09-07 00:29:01 +0900

    Added: ClearDB config to production/db.php

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -3,12 +3,14 @@
  * The production database settings. These get merged with the global settings.
  */
 
+$cleardb = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => sprintf("mysql:host=%s;dbname=%s", $cleardb['host'], substr($cleardb['path'], 1)),
+			'username'   => $cleardb['user'],
+			'password'   => $cleardb['pass'],
 		),
 	),
 );
