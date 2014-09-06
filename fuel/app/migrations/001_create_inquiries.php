1d1e4388c20e1c39eb631fccc9a6ebb58a056696
mallowlabs <mallowlabs@gmail.com>
2014-09-07 00:09:30 +0900

    Fixed: for SQLite3

--- fuel/app/migrations/001_create_inquiries.php
+++ fuel/app/migrations/001_create_inquiries.php
@@ -6,15 +6,26 @@ class Create_inquiries
 {
 	public function up()
 	{
-		\DBUtil::create_table('inquiries', array(
-			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-			'name' => array('constraint' => 255, 'type' => 'varchar'),
-			'email' => array('constraint' => 255, 'type' => 'varchar'),
-			'body' => array('type' => 'text'),
-			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
-			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+		if (\Fuel::$env == \Fuel::PRODUCTION) {
+			\DBUtil::create_table('inquiries', array(
+				'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+				'name' => array('constraint' => 255, 'type' => 'varchar'),
+				'email' => array('constraint' => 255, 'type' => 'varchar'),
+				'body' => array('type' => 'text'),
+				'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+				'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+			), array('id'));
+		} else {
+			\DBUtil::create_table('inquiries', array(
+				'id' => array('type' => 'integer primary key', 'autoincrement' => true,),
+				'name' => array('type' => 'text'),
+				'email' => array('type' => 'text'),
+				'body' => array('type' => 'text'),
+				'created_at' => array('type' => 'integer', 'null' => true),
+				'updated_at' => array('type' => 'integer', 'null' => true),
+			));
+		}
 
-		), array('id'));
 	}
 
 	public function down()
